22beb81c072db45d03c468836940cc2ef8b1f28e
Jojomaps <fjoelle14@gmail.com>
2025-04-10 18:35:59 +0200

    Ajout methode BDD

--- Modele/Modele_hpp/BaseDonnees.hpp
+++ Modele/Modele_hpp/BaseDonnees.hpp
@@ -3,11 +3,14 @@
 
 #include <sqlite3.h>
 #include <iostream>
+#include <stdexcept>
 #include "Utilisateur.hpp"
 #include "Trajet.hpp"
 #include "Avis.hpp"
 #include "Paiement.hpp"
 #include "Reservation.hpp"
+#include "Conducteur.hpp"
+#include "Passager.hpp"
 
 class Database {
 	
@@ -18,6 +21,30 @@ class Database {
 
     	bool execute(const std::string& query);
     	void initTables();  // Déclaration de la fonction d'initialisation
+        void ajouterUtilisateur(std::string nom, std::string prenom, std::string email, std::string mdp, std::string adressePostale, bool fumer);
+        Utilisateur getUtilisateurByEmailAndMDP(std::string email, std::string mdp);
+        void ajouterTrajet(int idTrajet, std::string date, std::string heureDepart, std::string heureArrivee, std::string lieuDepart, std::string lieuArrivee, std::vector<std::pair<std::string, float>> segmentsPrix, std::vector<std::string> villesEtapes, bool disponible, bool allerRetour, bool animaux, std::string voiture, int nombrePlaceDispo, bool etat, float emissionCO2, std::string description);
+        void ajouterReservation(float prix, int ID, bool statut);
+        void ajouterAvis(int note, std::string commentaire, std::string typeAvis);
+        Trajet getTrajetByVilleDepartEtArrivee(std::string villeDepart, std::string villeArrivee);
+        Reservation getReservationByID(int ID);
+        float getEmissionCO2ByIdTraget(int IDTrajet);
+        vector<pair<string, float>> getPrixByIdTrajet(int IDTrajet);
+        vector<std::string> getPointIntermediaireByIdTrajet(int IDTrajet);
+        std::string getDateDepatByIdTrajet(int IDTrajet);
+        std::string getDateArriveeByIdTrajet(int IDTrajet);
+        std::string getHeureArriveeByIdTrajet(int IDTrajet);
+        std::string getHeureDepartByIdTrajet(int IDTrajet);
+        std::string getVilleArriveeByIdTrajet(int IDTrajet);
+        std::string getVilleDepartByIdTrajet(int IDTrajet);
+        Avis getAvisByIdTrajet(int IDTrajet);
+        Conducteur getConducteurByID(int idUtilisateur);
+        Passager getPassagerByID(int idUtilisateur);
+
+
+
+
+
 };
 
 #endif
--- Modele/Modele_hpp/Conducteur.hpp
+++ Modele/Modele_hpp/Conducteur.hpp
@@ -4,7 +4,7 @@
 #include <string>
 #include "Utilisateur.hpp"
 
-class Conducteur{
+class Conducteur : public Utilisateur{
 
   public:
     Conducteur(int idUtilisateur, std::string nom, std::string prenom, std::string email, std::string motPasse,std::string adressePostale, bool fumeur);
