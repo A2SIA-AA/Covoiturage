de578eb045ee018c59bad281057e8f804087110c
A2SIA-AA <assia.ait_taleb@insa-rouen.fr>
2025-04-22 12:17:51 +0200

    Modification Controller + Ajout AccueilInterface

---
+++ Controlleur/Controlleur_hpp/AccueilControlleur.hpp
@@ -0,0 +1,28 @@
+#ifndef ACCUEILCONTROLLEUR_H
+#define ACCUEILCONTROLLEUR_H
+
+class AccueilControlleur
+{
+public:
+    /**
+    * @brief Redirige l'utilisateur vers "ModifierProfilInterface"
+    */
+    void TraiterModifierProfil();
+
+    /**
+     * @brief Redirige l'utilisateur vers "RechercheInterface"
+     */
+    void TraiterRecherche();
+
+    /**
+     * @brief Redirige l'utilisateur vers "MesReservationInterface"
+     */
+    void TraiterMesReservation();
+
+    /**
+     * @brief Redirige l'utilisateur vers "MesAnnoncesInterface"
+     */
+    void TraiterMesAnnonces();
+};
+
+#endif //ACCUEILCONTROLLEUR_H
