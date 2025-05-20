e532904727009d4b3198a732e8e648e4577350ec
A2SIA-AA <assia.ait_taleb@insa-rouen.fr>
2025-05-20 15:57:32 +0100

    modif de InscriptionControlleur.cpp et AccueilControlleur.cpp et InscriptionControlleur.hpp

--- Controlleur/Controlleur_cpp/InscriptionControlleur.cpp
+++ Controlleur/Controlleur_cpp/InscriptionControlleur.cpp
@@ -10,10 +10,10 @@ bool InscriptionControlleur::verifierSaisieInscription(
     std::string prenom,
     std::string email,
     std::string motPasse,
-    std::string numeroTelephone,
+    std::string adressePostal,
     bool fumeur) {
     // Vérifier que tous les champs sont remplis
-    if (nom.empty() || prenom.empty() || email.empty() || motPasse.empty() || numeroTelephone.empty() || (fumeur != false && fumeur != true)) {
+    if (nom.empty() || prenom.empty() || email.empty() || motPasse.empty() || adressePostal.empty() || (fumeur != false && fumeur != true)) {
         return false;
     }
     // Vérification basique du format d'email
@@ -29,7 +29,7 @@ bool InscriptionControlleur::verifierSaisieInscription(
     }
     // Vérifier que le numéro de téléphone contient uniquement des chiffres
     const std::regex phonePattern(R"(^\d+$)");
-    if (!std::regex_match(numeroTelephone, phonePattern)) {
+    if (!std::regex_match(adressePostal, phonePattern)) {
         return false;
     }
     return true;
@@ -40,10 +40,10 @@ void InscriptionControlleur::traiterInscription(
     std::string prenom,
     std::string email,
     std::string motPasse,
-    std::string numeroTelephone,
+    std::string adressePostal,
     bool fumeur) {
-    if (verifierSaisieInscription(nom, prenom, email, motPasse, numeroTelephone,fumeur)) {
-        Utilisateur utilisateur(nom, prenom, email, motPasse, numeroTelephone,fumeur);
+    if (verifierSaisieInscription(nom, prenom, email, motPasse, adressePostal,fumeur)) {
+        Utilisateur utilisateur(nom, prenom, email, motPasse, adressePostal,fumeur);
         Database database;
         database.ajouterUtilisateur(utilisateur);
         std::cout << "Inscription réussie ! Vous pouvez désormais vous connecter." << std::endl;
