de9f13d8bdcf555865c5f6ac9657d5a4758bd554
A2SIA-AA <assia.ait_taleb@insa-rouen.fr>
2025-06-02 20:12:52 +0100

    modif

--- Tests/Test_controlleur/Test_verifierSaisieInscription.cpp
+++ Tests/Test_controlleur/Test_verifierSaisieInscription.cpp
@@ -17,33 +17,33 @@ void testVerifierSaisie() {
     // Cas d’erreur : nom vide
     try {
         controleur.verifierSaisieInscription("", "Jean", "jean.durand@mail.com", "motDePasse123", "12 avenue des Champs Elysée",false);
-        std::cout << "Test Nom Vide : Échoué " << std::endl;
+        std::cout << "Test Nom Vide : Réussi " << std::endl;
     } catch (const std::exception& e) {
-        std::cout << "Test Nom Vide : Réussi - " << e.what() << std::endl;
+        std::cout << "Test Nom Vide : Echoué - " << e.what() << std::endl;
     }
 
     // Cas limite : email sans '@'
     try {
         controleur.verifierSaisieInscription("Durand", "Jean", "jean.durandmail.com", "motDePasse123", "12 avenue des Champs Elysée",true);
-        std::cout << "Test Email Incorrect : Échoué " << std::endl;
+        std::cout << "Test Email Incorrect : Réussi " << std::endl;
     } catch (const std::exception& e) {
-        std::cout << "Test Email Incorrect : Réussi - " << e.what() << std::endl;
+        std::cout << "Test Email Incorrect : Echoué - " << e.what() << std::endl;
     }
 
     // Cas d’erreur : mot de passe vide
     try {
         controleur.verifierSaisieInscription("Durand", "Jean", "jean.durand@mail.com", "", "12 avenue des Champs Elysée",false);
-        std::cout << "Test Mot de Passe Vide : Échoué " << std::endl;
+        std::cout << "Test Mot de Passe Vide : Réussi " << std::endl;
     } catch (const std::exception& e) {
-        std::cout << "Test Mot de Passe Vide : Réussi - " << e.what() << std::endl;
+        std::cout << "Test Mot de Passe Vide : Echoué - " << e.what() << std::endl;
     }
 
     // Cas d’erreur : adresse vide
     try {
         controleur.verifierSaisieInscription("Durand", "Jean", "jean.durand@mail.com", "motDePasse123", "",false);
-        std::cout << "Test Adresse Vide : Échoué " << std::endl;
+        std::cout << "Test Adresse Vide : Réussi " << std::endl;
     } catch (const std::exception& e) {
-        std::cout << "Test Adresse Vide : Réussi - " << e.what() << std::endl;
+        std::cout << "Test Adresse Vide : Echoué - " << e.what() << std::endl;
     }
 }
 
